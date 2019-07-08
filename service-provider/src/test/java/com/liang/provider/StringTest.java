1dbd216e3657796351be35173e1ca91e81b8801b
max-translia <32696645+max-translia@users.noreply.github.com>
2019-07-08 09:19:49 +0800

    添加测试类

---
+++ service-provider/src/test/java/com/liang/provider/StringTest.java
@@ -0,0 +1,16 @@
+package com.liang.provider;
+
+public class StringTest {
+
+
+    public static void main(String[] args) {
+        String a = "a";
+
+        String b = "b";
+
+        String c = a+b;
+
+        System.out.println(c);
+
+    }
+}
